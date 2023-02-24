de225c23f1fb2c79382b0955874486a29b1469f0
AlmazTATAR <112821309+AlmazBavly@users.noreply.github.com>
2023-02-24 14:21:02 +0000

    Задание 4.1.17

--- main.cs
+++ main.cs
@@ -4,30 +4,35 @@ class MainClass {
   public static void Main (string[] args) {
 		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-		var color = Console.ReadLine();
+		switch (Console.ReadLine())
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-		switch(color)
-		{
-			case "red":
-				Console.BackgroundColor = ConsoleColor.Red;
-				Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+                break;
 
-				Console.WriteLine("Your color is red!");
-			  break;
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-			case "green":
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is green!");
+                break;
 
-				Console.WriteLine("Your color is green!");
-			  break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-			default:
-				Console.BackgroundColor = ConsoleColor.Cyan;
-				Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is cyan!");
+                break;
 
-				Console.WriteLine("Your color is cyan!");
-			  break;
-		}
-  }
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+    }
 }
